28abc33f75d55a048a5572f9850fe5dc3364dc74
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-02-18 21:38:00 +0530

    Boilerplate

---
+++ practice/src/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define rep(i, a, b) for (auto i = (a); i < (b); ++i)
+#define per(i, a, b) for (auto i = (b); i-- > (a); )
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+void init() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+}
+
+void solve() {
+	cout << "Hello, World!" << endl;
+}
+
+int main() {
+	init();
+	int t = 1;
+	cin >> t;
+	while (t-- > 0) {
+		solve();
+	}
+	return 0;
+}
